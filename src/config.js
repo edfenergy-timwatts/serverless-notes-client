559752391eaa21dcdacdefc1dd8263532a31269f
Tim Watts <tim.watts@edfenergy.com>
2020-01-24 11:56:01 +0000

    Landing page and minimal navigation

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "bucket1-timwatts"
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://4kvfohqoq2.execute-api.eu-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_cFg6rpXwl",
+    APP_CLIENT_ID: "765ldg74q80q0109cl6ni8aper",
+    IDENTITY_POOL_ID: "eu-west-2:3f685fd0-8d56-4b05-821f-130d757fc6af"
+  }
+};
